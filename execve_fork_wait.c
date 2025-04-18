e4e8b6a1714a9547119125c63f4def406f4adc45
QuentinC26 <q.zuzlewski@gmail.com>
2025-04-18 16:35:24 +0200

    add a code sauvegarde

---
+++ execve_fork_wait.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+/**
+ * main - fork & wait example
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+	return (0);
+}
